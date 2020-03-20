a9269991e01a6ffa08f729984b416244c58ff68b
tdxa <anndoman3@gmail.com>
2020-03-20 18:26:45 +0100

    Addition of the GameBoard class and implementation of the board with initial values

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -8,9 +8,12 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 
 public class Controller implements Initializable {
+
+    GameBoard gameBoard;
 
     @FXML Button btn_solve;
     @FXML Button btn_reset;
@@ -19,12 +22,14 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL arg0, ResourceBundle arg1){
 
+        gameBoard = new GameBoard();
         GraphicsContext context = canvas.getGraphicsContext2D();
         drawOnCanvas(context);
     }
 
     private void drawOnCanvas(GraphicsContext context){
 
+        //drawing empty board
         context.clearRect(0, 0, 450, 450);
         for(int row = 0; row<9; row++) {
             for(int col = 0; col<9; col++) {
@@ -38,5 +43,28 @@ public class Controller implements Initializable {
             }
         }
 
+        //drawing the initial numbers from GameBoard class
+
+        int[][] initial = gameBoard.getStarting();
+        for(int row = 0; row<9; row++){
+            for(int col = 0; col<9; col++){
+
+                int position_y = row * 50 + 35;
+                int position_x = col * 50 + 20;
+
+                context.setFill(Color.BLACK);
+                context.setFont(new Font(20));
+
+                if(initial[row][col] != 0){
+                    context.fillText(initial[row][col] + "",position_x,position_y);
+                }
+
+            }
+        }
+
+
+
     }
+
+
 }
